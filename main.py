bf18b944ca53b110ab43cc34a082e009b5282d88
leeukleegmail <leeuklee@gmail.com>
2022-07-21 21:17:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import BME280
+from utime import sleep
+import ssd1306
+from machine import Pin, I2C
+
+
+i2c = I2C(scl=Pin(5), sda=Pin(4))
+
+bme = BME280.BME280(i2c=i2c) # addr 118
+display = ssd1306.SSD1306_I2C(128, 64, i2c) # addr 60
+pin = Pin(14, Pin.IN, Pin.PULL_UP)
+
+
+while True:
+    display.fill(0)
+    display.show()
+    if not pin.value():
+        display.text('Temp:{}'.format(bme.temperature[:-1]), 10, 10)
+        display.text('Hum:{}'.format(bme.humidity[:-1]), 10, 26)
+        display.text('Pres:{}'.format(bme.pressure[:-3]), 10, 42)
+        display.rect(0, 0, 128, 64, 1)
+        display.show()
+        sleep(5)
